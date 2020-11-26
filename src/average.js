90b540011ec94aa90d8fff77f6e1189710f890d3
Ruan Miguel <ruanmiguelps@gmail.com>
2020-11-26 16:29:29 -0300

    codeclimate

--- src/average.js
+++ src/average.js
@@ -15,17 +15,16 @@
 const average = (array) => {
 if (array.length === 0) return undefined
   let total = 0;
-for (let index =0; index<array.length; index+=1){
-    if (typeof array[index] !== 'number' ){
-        return undefined
-      } else {
-    total += array[index]
-}
-}
-let avg = Math.round(total/array.length)
+for (let index = 0; index < array.length; index += 1) {
+  if (typeof array[index] !== 'number') {
+      return undefined;
+    } {
+      total += array[index];
+    }
+  }
+const avg = Math.round (total/array.length)
 
-return avg
-
+  return avg;
 };
 
 module.exports = average;
